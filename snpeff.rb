f312d2ba497a5b7e6c2d1c50ad465e4c4d6da7d2
chapmanb <chapmanb@50mail.com>
2014-01-03 07:40:32 -0500

    snpEff: update to 3.4e, enhance shell script Upgrades snpEff to the latest version and also enhances the shell wrapper script to identify java memory and property parameters. This allows tuning of memory usage from the command line, which is fairly important for snpEff on large genomes.

--- snpeff.rb
+++ snpeff.rb
@@ -2,19 +2,43 @@ require 'formula'
 
 class Snpeff < Formula
   homepage 'http://snpeff.sourceforge.net/'
-  url 'http://downloads.sourceforge.net/project/snpeff/snpEff_v3_3_core.zip'
-  version '3.3H'
-  sha1 'ae69eee82e1c5a859c1fd0eb185cf2d5c81b38ba'
+  url 'http://downloads.sourceforge.net/project/snpeff/snpEff_v3_4_core.zip'
+  version '3.4e'
+  sha1 '35207a3b3b14b3a2faba4e833f59251401bc8f1c'
 
   def install
     java = share / 'java'
     java.install 'snpEff.jar', 'SnpSift.jar', 'snpEff.config'
     # Install a shell script to launch snpEff.
+    # Identifies java-specific memory and property parameters.
     bin.mkdir
     open(bin / 'snpeff', 'w') do |file|
       file.write <<-EOS.undent
-        #!/bin/sh
-        exec java -jar #{java}/snpEff.jar "$@" -c #{java}/snpEff.config
+        #!/bin/bash
+        default_jvm_mem_opts="-Xms512m -Xmx1g"
+        jvm_mem_opts=""
+        jvm_prop_opts=""
+        pass_args=""
+        for arg in "$@"; do
+            case $arg in
+                '-D'*)
+                    jvm_prop_opts="$jvm_prop_opts $arg"
+                    ;;
+                 '-Xm'*)
+                    jvm_mem_opts="$jvm_mem_opts $arg"
+                    ;;
+                 *)
+                    pass_args="$pass_args $arg"
+                    ;;
+            esac
+        done
+        if [ "$jvm_mem_opts" == "" ]; then
+            jvm_mem_opts="$default_jvm_mem_opts"
+        fi
+        if [ "$pass_args" != "" ]; then
+            pass_args="$pass_args -c #{java}/snpEff.config"
+        fi
+        exec java $jvm_mem_opts $jvm_prop_opts -jar #{java}/snpEff.jar $pass_args
       EOS
     end
   end
@@ -22,7 +46,7 @@ class Snpeff < Formula
   def caveats
     puts <<-EOS.undent
       Download the human database using the command
-          snpeff download -v GRCh37.71
+          snpeff download -v GRCh37.74
       The databases will be installed in ~/snpEff/data
     EOS
   end
