1c21cf62e7a7a4ca0299f6d24d1c624588ba8283
chapmanb <chapmanb@50mail.com>
2014-04-09 05:53:09 -0400

    delly: bump version to 0.3.3 Moves to statically linked binary for delly on Linux and drops compilation, since build has not been building reliably across multiple systems due to dependencies. Loses OSX support since static binaries are not available.

--- delly.rb
+++ delly.rb
@@ -1,44 +1,25 @@
 require 'formula'
 
 class Delly < Formula
-  homepage 'http://www.embl.de/~rausch/delly.html'
-  url 'http://www.embl.de/~rausch/delly_source_v0.0.11.tar.gz'
-  sha1 'd0e1cd95a0d526e308c5aff88c19212e558d9a1f'
+  homepage 'https://github.com/tobiasrausch/delly'
+  version '0.3.3'
+  url 'https://github.com/tobiasrausch/delly/releases/download/v0.3.3/delly_v0.3.3_linux_x86_64bit'
+  sha1 '50db4727dc5d163338d59b48f79d9f7041b2bfd9'
 
-  depends_on 'bamtools'
-  depends_on 'boost'
-
-  def patches
-    # Allows Makefile to find boost and bamtools within Homebrew hierarchy
-    DATA
+  resource 'delly-source' do
+    url 'https://github.com/tobiasrausch/delly/archive/v0.3.3.tar.gz'
+    sha1 'b3537ee3276784356019fbb5c1b624d64f82469e'
   end
 
   def install
-    cd 'pemgr' do
-      inreplace 'Makefile', 'LDFLAGS += --static', ''
-      inreplace 'Makefile', /(-lboost[^ ]*)/, '\1-mt' if OS.mac?
-      system 'make', "CXX=#{ENV.cxx}"
-      bin.install %w{delly/delly duppy/duppy invy/invy jumpy/jumpy}
+    raise 'delly not yet supported for MacOSX' if OS.mac?
+    bin.install 'delly_v0.3.3_linux_x86_64bit' => 'delly'
+    resource('delly-source').stage do
+      (share/'delly').install Dir['src/*.py']
     end
-    doc.install 'README'
   end
 
   test do
     system 'delly 2>&1 |grep -q delly'
   end
 end
-__END__
---- a/pemgr/Makefile	2013-06-06 16:19:51.000000000 -0400
-+++ b/pemgr/Makefile	2013-10-17 06:29:19.132265246 -0400
-@@ -1,8 +1,8 @@
--BOOST=/g/solexa/bin/software/boost_1_53_0
--BAMTOOLS=/g/solexa/bin/software/bamtools/
-+BOOST=HOMEBREW_PREFIX
-+BAMTOOLS=HOMEBREW_PREFIX
- 
- CXX=g++
--CXXFLAGS += -isystem ${BOOST}/include -isystem ${BAMTOOLS}/include -I../torali
-+CXXFLAGS += -isystem ${BOOST}/include -isystem ${BAMTOOLS}/include/bamtools -I../torali
- CXXFLAGS += -O9 -pedantic -W -Wall
- 
- ### Valgrind
