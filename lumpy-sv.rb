94bd6a63a98de8b79a5eb5500c4ede4af6a19ce8
chapmanb <chapmanb@50mail.com>
2015-07-13 07:06:55 -0400

    lumpy-sv: v0.2.11
    Updates to the latest lumpy release:
    - Includes support for new lumpyexpress script.
    - Update dependencies based on current suggestions.

--- lumpy-sv.rb
+++ lumpy-sv.rb
@@ -2,8 +2,13 @@ class LumpySv < Formula
   homepage "https://github.com/arq5x/lumpy-sv"
   # doi "10.1186/gb-2014-15-6-r84"
   # tag "bioinformatics"
-  url "https://github.com/arq5x/lumpy-sv/releases/download/0.2.9/lumpy-sv-0.2.9.tar.gz"
-  sha1 "3ca24aed2b5a57a5e8a55f2f65057520474fb77c"
+  url "https://github.com/arq5x/lumpy-sv/releases/download/0.2.11/lumpy-sv-0.2.11.tar.gz"
+  sha1 "ad4bbd5054d1a98fa2974217459bd61e0d5f255f"
+
+  resource "bamkit" do
+    url "https://github.com/cc2qe/bamkit/archive/6b8c20.zip"
+    sha1 "b70bd6749247558bdf94ed258491e1712034cb51"
+  end
 
   bottle do
     root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
@@ -13,21 +18,27 @@ class LumpySv < Formula
     sha1 "9a951d727f67fc9a315d26a260d0ad4f1b69cd6f" => :mountain_lion
   end
 
-  depends_on "bamtools" => :recommended
-  depends_on "samtools" => :recommended
-  depends_on "bedtools" => :recommended
+  depends_on "samblaster" => :optional
+  depends_on "samtools" => :optional
+  depends_on "sambamba" => :optional
   depends_on "bwa" => :optional
-  depends_on "novoalign" => :optional
-  depends_on "yaha" => :optional
 
   def install
     ENV.deparallelize
+    inreplace 'scripts/lumpyexpress.config' do |s|
+      s.change_make_var! "LUMPY_HOME", "#{HOMEBREW_PREFIX}/share/lumpy-sv"
+    end
     system "make"
     bin.install "bin/lumpy"
-    (share/"lumpy-sv").install Dir["scripts/*"]
+    bin.install Dir['scripts/lumpyexpress*']
+    (share/"lumpy-sv/scripts").install Dir["scripts/*"]
+    resource("bamkit").stage {
+      (share/"lumpy-sv/scripts/bamkit").install Dir["*"]
+    }
   end
 
   test do
     system "#{bin}/lumpy 2>&1 |grep -q structural"
+    system "#{bin}/lumpyexpress 2>&1 |grep -q lumpyexpress"
   end
 end
